428dbc1e07b55ad24e19d8d616409ce2be9cfe3d
Matheus Gadelha <matheusabrantesgadelha@gmail.com>
2017-04-04 17:39:44 -0400

    Adding spaces to includes and changing order.

--- example1.cpp
+++ example1.cpp
@@ -1,9 +1,10 @@
-#include"cnpy.h"
-#include<complex>
-#include<cstdlib>
-#include<iostream>
-#include<map>
-#include<string>
+#include <complex>
+#include <cstdlib>
+#include <iostream>
+#include <map>
+#include <string>
+
+#include "cnpy.h"
 
 const int Nx = 128;
 const int Ny = 64;
